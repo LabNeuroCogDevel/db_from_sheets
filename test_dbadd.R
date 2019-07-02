10ecd98a3e224c524195d267e703cb9bdf2d7b26
WillForan <willforan@gmail.com>
2019-07-02 14:59:21 -0400

    add eid visit date tests

--- test_dbadd.R
+++ test_dbadd.R
@@ -5,6 +5,7 @@
 
 
 library(testthat)
+library(lubridate)
 # run testthat if run like ./test_dbadd.R
 # alternative: Rscript -e 'testthat::test_dir(".")'
 if (sys.nframe() == 0) testthat::test_dir(".")
@@ -62,4 +63,31 @@ test_that("visit type counts", {
    expect_true(all(!is.na(vs$Scan)))
    # P5 has no behaviorals
    expect_true(all(!is.na(vs$Behavioral[vs$study!="P5"])))
+})
+
+test_that("have cal eid", {
+   cal <- tbl(con, "visit") %>%
+      inner_join(tbl(con, "visit_study")) %>%
+      filter(!is.na(googleuri)) %>%
+      group_by(study) %>%
+      summarise(n=n(),
+                newest=max(vtimestamp, na.rm=T),
+                oldest=min(vtimestamp, na.rm=T)) %>%
+      collect
+
+   expect_gte(cal$n[cal$study == "PET"], 548)
+   expect_gte(cal$n[cal$study == "P5"], 57)
+   expect_gte(cal$n[cal$study == "BrainMechR01"], 244)
+
+   # earliest date
+   expect_true(cal$oldest[cal$study == "BrainMechR01"]  < ymd("20180125"))
+   expect_true(cal$oldest[cal$study == "P5"]  < ymd("20160216"))
+   expect_true(cal$oldest[cal$study == "PET"] < ymd("20180124"))
+
+   # most recent date -- test will be out of date
+   expect_true(cal$newest[cal$study == "BrainMechR01"]  >= ymd("20190702"))
+   expect_true(cal$newest[cal$study == "P5"]  >= ymd("20171229"))
+   # why is pet so far behind?
+   # TODO: fix PET dates
+   expect_true(cal$newest[cal$study == "PET"]  >= ymd("20190320"))
 })
