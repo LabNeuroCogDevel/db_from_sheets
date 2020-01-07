4d1dc52ee3a7496f8e3304ad088ca69a60cd2a0f
WillForan <willforan@gmail.com>
2020-01-07 16:33:57 -0500

    include lunaid in comments

--- manual_updates.sql
+++ manual_updates.sql
@@ -3,6 +3,7 @@ update person set sex='M' where pid=1411;
 update person set sex='F' where pid=1388;
 -- repeated lunaids
 update enroll set id='11748' where id like '11748; 11515';
+--  before getting extra id 11653
 insert into enroll (pid,etype,id,edate) values (1346,'LunaID','11653','2018-04-30') ON CONFLICT DO NOTHING;
 
 
