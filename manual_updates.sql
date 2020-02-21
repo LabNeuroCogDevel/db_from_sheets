1e91314a5ef801b04e721cc3ce5e5c255c85634c
WillForan <willforan@gmail.com>
2020-02-21 09:26:27 -0500

    ✨ add missing BIRC/NIC ids

--- manual_updates.sql
+++ manual_updates.sql
@@ -15,3 +15,22 @@ select v.vid, e.eid
        and e.edate = v.vtimestamp::date
   where etype like 'PETID'
    and ve.vid is null;
+
+-- missing birc/nic ids
+drop table if exists missing_temp;
+create temp table  missing_temp as
+with m (ld8, nic) as (values
+ ('10161_20120517','120517160603'),
+ ('10216_20160113','160113170726'),
+ ('10173_20130523','130523154926'),
+ ('10344_20101214','101214545011'),
+ ('10202_20180319','180319154612')) 
+select v.pid,
+ vid,ld8,
+ vtimestamp::date as edate,
+ nic,
+ 'BIRC' as etype
+ from visits_view v join m on m.ld8 like v.lunaid||'_'||v.ymd left join enroll e on e.id= m.nic where e.id is null;
+
+insert into enroll (pid, id, etype, edate) select pid, nic, etype, edate from missing_temp;
+insert into visit_enroll (vid, eid) select vid, eid from missing_temp t join enroll e on e.id=t.nic;
